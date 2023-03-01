0a5c6d5ec240ee398cdc4ae784dd868862f22341
EetuHuttula <100779993+EetuHuttula@users.noreply.github.com>
2023-03-01 12:17:33 +0200

    Homepage started

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './CSS/home.css'
-
+import homeimg from './Homeimg.png'
 export default function Home() {
   return (
-    <div className="Container">
-      <div className="Cover_gradient">
-          lol
+    <div className="container home_cont">
+      <div className="row">
+        <div className="inffobox">
+        <h3>Tervetuloa <span>EetuOy</span> sivuille</h3>
+        <p>Tämä sivusto on tehty omaksi ilokseni ja hiodakseni omia koodaus taitoja.</p>
+        </div>
+        <div className="image_section"> {/**Kuva <a href="https://pixabay.com/fi/users/elf-moondance-19728901/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=6631394">Moondance</a> <a href="https://pixabay.com/fi//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=6631394">Pixabaystä</a> */}
+           <img className="home_Image" src={homeimg} />
+        </div>
       </div>
     </div>
   )
